deffd5ca532fd4d8c70afa9455c1dbc5c013dcbd
Gabriel.Deon <gabriel.deon@compasso.com.br>
2025-04-29 18:28:41 -0300

    fix: Add Prisma Service to item-pedido providers

--- src/item-pedido/item-pedido.module.ts
+++ src/item-pedido/item-pedido.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ItemPedidoService } from './item-pedido.service';
 import { ItemPedidoController } from './item-pedido.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
-  providers: [ItemPedidoService],
+  providers: [ItemPedidoService, PrismaService],
   controllers: [ItemPedidoController]
 })
 export class ItemPedidoModule {}
